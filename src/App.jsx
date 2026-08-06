ac61d7e5a3f4426061bb003bf7b51982a74bf37f
Faiz ali <faizalidesk@gmail.com>
2026-08-06 09:32:03 +0700

    10

--- src/App.jsx
+++ src/App.jsx
@@ -2,83 +2,35 @@ import {
   BrowserRouter,
   Routes,
   Route
+} from "react-router-dom";
+
+import LandingPage from "./pages/LandingPage";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Dashboard from "./pages/dashboard/Dashboard";
+import ProtectedRoute from "./routes/ProtectedRoute";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/register" element={<Register />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-from "react-router-dom";
-
-import LandingPage
-from "./pages/LandingPage";
-
-import Login
-from "./pages/auth/Index";
-
-import Register
-from "./pages/auth/Register";
-
-import Dashboard
-from "./pages/dashboard/Dashboard";
-
-import ProtectedRoute
-from "./routes/ProtectedRoute";
-
-
-
-function App(){
-
-
-return (
-
-<BrowserRouter>
-
-
-<Routes>
-
-
-<Route
-path="/"
-element={<LandingPage/>}
-/>
-
-
-<Route
-path="/login"
-element={<Login/>}
-/>
-
-
-<Route
-path="/register"
-element={<Register/>}
-/>
-
-
-
-<Route
-
-path="/dashboard"
-
-element={
-
-<ProtectedRoute>
-
-<Dashboard/>
-
-</ProtectedRoute>
-
-}
-
-/>
-
-
-
-</Routes>
-
-
-</BrowserRouter>
-
-);
-
-
-}
-
 
 export default App;
