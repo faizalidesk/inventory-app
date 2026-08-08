2a34b554b3cc59dd01512b6dcee92e939129e0c8
faizalidesk <faizalidesk@gmai.com>
2026-08-08 18:30:14 +0700

    s

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import ProtectedRoute from "./routes/ProtectedRoute";
 import PublicOnlyRoute from "./routes/PublicOnlyRoute";
 
 // Set to true to lock the entire site under maintenance mode
-const IS_MAINTENANCE = true;
+const IS_MAINTENANCE = false;
 
 function App() {
   if (IS_MAINTENANCE) {
